a54f5a306d503438488106efa9afadb75f92da84
clarenz kent reyes <clarktoto51@gmail.com>
2026-02-09 14:34:41 +0800

    deploy

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const path = require("path");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
